665b3a6e6bacefa32c61dc5d1dfda049bc6475a9
SammaySarkar <smmysrkr@gmail.com>
2016-09-04 19:42:48 +0600

    renamed `filename_filter` setting...
    to `build_on_save_filetypes`

--- BuildOnSave.py
+++ BuildOnSave.py
@@ -7,5 +7,5 @@ class BuildOnSave(sublime_plugin.EventListener):
         prefs = sublime.load_settings("BuildOnSave.sublime-settings")
         if view.settings().get('build_on_save', True):
             file_ext = view.file_name().split('.')[-1]
-            if file_ext in prefs.get("filename_filter"):
+            if file_ext in prefs.get("build_on_save_filetypes"):
                 view.window().run_command("build")

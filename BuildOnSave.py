3d2b042b319419a4fff3b2917dcfb2855fdd7877
James Doyle <james2doyle@gmail.com>
2025-03-14 12:17:12 -0700

    Changed: refactor to use syntax and mapping to specific build command

--- BuildOnSave.py
+++ BuildOnSave.py
@@ -1,32 +1,98 @@
 import sublime
 import sublime_plugin
-
+import os
 
 class BuildOnSave(sublime_plugin.EventListener):
     """the main command class"""
 
     def on_post_save(self, view):
-        """triggers after save"""
-        prefs = sublime.load_settings("BuildOnSave.sublime-settings")
-        if view.settings().get('build_on_save', True):
-            file_ext = view.file_name().split('.')[-1]
-            if file_ext in prefs.get("build_on_save_filetypes"):
-                view.window().run_command("build")
+        # Get project data (if available)
+        project_data = view.window().project_data()
 
+        # --- Get Settings, with Project Settings Overriding Global ---
+        if project_data and "settings" in project_data and "build_on_save" in project_data["settings"]:
+            build_on_save_settings = project_data["settings"]["build_on_save"]
+        else: #Fallback on Global Settings
+            settings = sublime.load_settings("BuildOnSave.sublime-settings")
+            build_on_save_settings = settings.get("build_on_save", {})
 
-class ToggleBuildOnSaveCommand(sublime_plugin.TextCommand):
-    """toggle settings"""
+        logging_enabled = build_on_save_settings.get("logging", False)  # Get logging setting
 
-    # HACK it works!
-    # TODO figure out ST plugin api
+        if not build_on_save_settings.get("enabled", True):
+            if logging_enabled:
+                print("BuildOnSave: Skipping. Plugin is disabled in the settings.")
+            return
 
-    def run(self, edit):
-        """runs when the command is called"""
-        if self.view.settings().get('build_on_save', True):
-            self.view.settings().set('build_on_save', False)
+        file_name = view.file_name()
+        if not file_name:
+            if logging_enabled:
+                print("BuildOnSave: No file name for the view.")
+            return
+
+        # Get the syntax file path
+        syntax_file = view.settings().get('syntax')
+        if not syntax_file:
+            if logging_enabled:
+                print("BuildOnSave: No syntax defined for the view.")
+            return
+
+        # Extract the syntax name
+        syntax_name = os.path.splitext(os.path.basename(syntax_file))[0]
+
+        if not syntax_name:
+            if logging_enabled:
+                print("BuildOnSave: No syntax name found for the view.")
+            return
+
+        logging_enabled = build_on_save_settings.get("logging", False)  # Get logging setting
+        delay = build_on_save_settings.get("delay", 0)
+        build_system_map = build_on_save_settings.get("build_system_map", {})
+
+        # doesn't seem to currently work...
+        show_panel = build_on_save_settings.get("show_panel_on_build", False) # Get setting
+
+        build_system_name = build_system_map.get(syntax_name)
+
+        if build_system_name:
+            def trigger_build():
+                always_run_default = build_on_save_settings.get("always_run_default", True)
+
+                # Prioritize project-defined build systems
+                if project_data and "build_systems" in project_data:
+                    for build_system in project_data["build_systems"]:
+                        # Found a matching build system in the project
+                        if "name" in build_system and build_system["name"] == build_system_name:
+                            # Construct the build command arguments
+                            build_args = {}
+                            for key, value in build_system.items():
+                                if key != "name":  # Exclude the 'name' key itself
+                                    if isinstance(value, str):
+                                        build_args[key] = sublime.expand_variables(value, view.window().extract_variables())
+                            # Conditionally add output-related arguments
+                            # doesn't seem to currently work...
+                            build_args["quiet"] = not show_panel
+
+                            if logging_enabled:
+                                print("BuildOnSave: Running \"{}\"".format(build_system_name))
+                            # Run the build command with the constructed arguments
+                            view.window().run_command("exec", build_args)
+
+                    if always_run_default:
+                        if logging_enabled:
+                            print("BuildOnSave: Running default build system for the view.")
+                        # Found a matching build system in the project
+                        view.window().run_command("build", {"variant": ""}) # Run the *default* build variant
+                        return  # Exit the function after triggering the build
+
+            # if logging_enabled:
+                # print("BuildOnSave: Running variant \"{}\"".format(build_system_name))
+            # If no matching project build system was found, fall back to variant
+            # view.window().run_command("build", {"variant": build_system_name})
+
+            if delay > 0:
+                sublime.set_timeout_async(trigger_build, delay)
+            else:
+                trigger_build()
         else:
-            self.view.settings().set('build_on_save', True)
-
-    def is_checked(self):
-        """updates menu item check mark"""
-        return self.view.settings().get('build_on_save', True)
+            if logging_enabled:
+                print("BuildOnSave: No build system found for syntax {}".format(syntax_name))
--- README.md
+++ README.md
@@ -4,13 +4,18 @@ BuildOnSave - SublimeText Plugin
 A plugin for [Sublime Text](http://www.sublimetext.com) that triggers the build
 command after each save.
 
+_A fork of [SublimeSaveOnBuild](https://github.com/alexnj/SublimeOnSaveBuild)_
+_A fork of [SublimeSaveOnBuild](https://github.com/bdeshi/sublime-BuildOnSave)_
+
+> This fork allows the user to pick which build system you want to run. It also uses Syntaxes for the mapping instead of file extensions.
+
 
 Installation
 ------------
 
 ### Package Control
   + Select **Package Control: Add Repository** from the Command Palette.
-  + Add `https://github.com/smsrkr/BuildOnSave`.
+  + Add `https://github.com/james2doyle/sublime-BuildOnSave`.
   + Select **Package Control: Install Package** from the Command Palette.
   + Search for and install `BuildOnSave`.
 
@@ -18,42 +23,59 @@ Installation
 + #### Linux/OSX
   ```batch
   cd "path/to/SublimeText/Packages/"
-  git clone git://github.com/smsrkr/BuildOnSave.git
+  git clone git://github.com/james2doyle/sublime-BuildOnSave.git
   ```
 
 + #### Windows
   ```batch
   cd "path\to\SublimeText\Packages\"
-  git clone git://github.com/smsrkr/BuildOnSave.git
+  git clone git://github.com/james2doyle/sublime-BuildOnSave.git
   ```
 
 
 Configuration
 -------------
 
-1. Edit the package setting `build_on_save_filetypes` if needed.<br>
-   This setting selects filetypes to build after save.
-2. You can control build-on-save for individual projects too. <br>
+1. You can control build-on-save for individual projects. <br>
    Simply configure the `.sublime-project` file, like so:
+
 ```json
 {
-  "settings":
-  {
-    "build_on_save": true
+  "settings": {
+    "build_on_save": {
+      // print details on what is happening
+      "logging": false,
+      // enable or disable build-on-save
+      "enabled": true,
+      // run the default build system when there is no match
+      "always_run_default": true,
+      // panel toggle
+      "show_panel_on_build": false,
+      // delay the build in case there is a reason
+      "delay": 0,
+      // Build-on-save will apply only to these file extensions
+      "build_system_map": {
+        // The keys are syntax names
+        // The values are the NAMES of Sublime Text build systems
+        // (as they appear in the Tools -> Build System menu).
+
+        // Examples:
+        // "HTML (Blade)": "Project - Format Blade File", // this is the "name" of a Project build system
+        // "JavaScript": "NodeJs", // this is the "name" of a build system
+        // "C++": "Make", // this is the "name" of a build system
+      }
+    }
   }
 }
 ```
-
 
 Usage
 -----
 1. Make sure your build systems are set up properly.
 2. Now just save a file, and the build command should run on it. <br>
-   (If the file's extension exists in `build_on_save_filetypes`.)
-3. Toggle Build-on-Save on a per file basis with **Tools | Build on Save**.
-
+   (If the file's syntax exists and a build system is found.)
 
 *Good Luck!*
 
-
 _A fork of [SublimeSaveOnBuild](https://github.com/alexnj/SublimeOnSaveBuild)_
+_A fork of [SublimeSaveOnBuild](https://github.com/bdeshi/sublime-BuildOnSave)_
